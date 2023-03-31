da64afd50289df976d9af2413c121a4dc8743f74
Anton Zhuravlev <79113636+INTERpol21@users.noreply.github.com>
2023-03-31 14:49:53 +0600

    v2

--- src/components/Bar/CustomBar.js
+++ src/components/Bar/CustomBar.js
@@ -1,40 +1,37 @@
 import React from 'react'
-import  './../../scss/CustomBar.scss'
+import './../../scss/CustomBar.scss'
 import { Scrollbars } from 'react-custom-scrollbars-2'
-
 
 const CustomBar = ({ setCoordinate, setZoom, places }) => {
   const handleClick = (place) => {
     setCoordinate([place.latitude, place.longitude])
-    setZoom(15)
+    setZoom(16)
   }
 
   return (
     <div className="sidebar">
-      <div className="sidebar__container">
-        <Scrollbars style={{ height: '95vh' }}>
-          {places.map((place) => {
-            return (
-              <div
-                className="sidebar__items"
-                key={place.latitude}
-                tabIndex={0}
-                onClick={() => handleClick(place)}>
-                <div className="sidebar__content_address">{place.address}</div>
-                <div className="sidebar__content_buttons">
-                  {place.budgets.map((budget, index) => {
-                    return (
-                      <div className="sidebar__content_button" key={budget + index}>
-                        {budget}
-                      </div>
-                    )
-                  })}
-                </div>
+      <Scrollbars style={{ height: '95vh' }}>
+        {places.map((place) => {
+          return (
+            <div
+              className="sidebar__items"
+              key={place.latitude}
+              tabIndex={0}
+              onClick={() => handleClick(place)}>
+              <div className="sidebar__content_address">{place.address}</div>
+              <div className="sidebar__content_buttons">
+                {place.budgets.map((budget) => {
+                  return (
+                    <div className="sidebar__content_button" key={budget}>
+                      {budget}
+                    </div>
+                  )
+                })}
               </div>
-            )
-          })}
-        </Scrollbars>
-      </div>
+            </div>
+          )
+        })}
+      </Scrollbars>
     </div>
   )
 }
